ed93fc65f663acf7fbdaadf6e9f791da6266fc44
yogaariyanto312 <yogaariyanto312@gmail.com>
2025-05-18 23:24:23 +0700

    commit pertama

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const texts = [
+   "Network Engineer.",
+   "Python Developer.",
+   "Electrical Engineer.",
+   "Web Developer."
+];
+
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+const speed = 100;
+
+function type() {
+   currentText = texts[i];
+   let displayedText = isDeleting
+      ? currentText.substring(0, j--)
+      : currentText.substring(0, j++);
+
+   document.getElementById("typewriter").innerHTML = displayedText;
+
+   if (!isDeleting && j === currentText.length) {
+      isDeleting = true;
+      setTimeout(type, 1500);
+   } else if (isDeleting && j === 0) {
+      isDeleting = false;
+      i = (i + 1) % texts.length;
+      setTimeout(type, 300);
+   } else {
+      setTimeout(type, isDeleting ? speed / 2 : speed);
+   }
+}
+
+type();
