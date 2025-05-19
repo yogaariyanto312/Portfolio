4c88d2d9360e86b184244e5306eb64867f2e1d82
yogaariyanto312 <yogaariyanto312@gmail.com>
2025-05-19 17:20:58 +0700

    mengubah tampilan btn menjadi lebih besar sedikit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// typewriter
 const texts = [
    "Network Engineer.",
    "Python Developer.",
@@ -14,21 +15,25 @@ const speed = 100;
 function type() {
    currentText = texts[i];
    let displayedText = isDeleting
-      ? currentText.substring(0, j--)
-      : currentText.substring(0, j++);
-
+   ? currentText.substring(0, j--)
+   : currentText.substring(0, j++);
+   
    document.getElementById("typewriter").innerHTML = displayedText;
-
+   
    if (!isDeleting && j === currentText.length) {
       isDeleting = true;
       setTimeout(type, 1500);
    } else if (isDeleting && j === 0) {
       isDeleting = false;
       i = (i + 1) % texts.length;
-      setTimeout(type, 300);
+      setTimeout(type, 100);
    } else {
-      setTimeout(type, isDeleting ? speed / 2 : speed);
+      setTimeout(type, isDeleting ? speed / 1 : speed);
    }
 }
 
 type();
+
+// end typewriter
+
+
